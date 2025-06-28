fb13203191c0aaf806ac475fef1a005b1e93b06c
Tai Le <161448595+blackicecoffee@users.noreply.github.com>
2025-06-28 11:09:08 +0700

    🔨 Add Naive RAG technique

--- rag/naive_rag.py
+++ rag/naive_rag.py
@@ -5,7 +5,27 @@ class NaiveRAG:
     def __init__(self, model: BaseLLM, vectordb: Collection):
         self.model = model
         self.vectordb = vectordb
+        self.pre_retrieval = None
+        self.post_retrieval = None
 
-    def generate(self, query: str):
-        pass
+    def retrieval(self, query: str) -> str:
+        retrieved_document = self.vectordb.query(
+            query_texts=[query],
+            n_results=5
+        )
+
+        context = ""
+        for idx, result in enumerate(retrieved_document["documents"][0], start=1):
+            context += f"----- Resource {idx} -----\n{result}\n"
+
+        return context
+
+    async def generate(self, query: str):
+        context = self.retrieval(query=query)
+
+        prompt = f"""Instructions: Base on the given context, answer the question of user\nContext:\n{context}\nQuestion: {query}\n"""
+
+        answer = await self.model.ainvoke(prompt)
+
+        return answer
     
